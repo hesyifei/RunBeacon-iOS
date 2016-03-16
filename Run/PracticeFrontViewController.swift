eb4e4791925dfa225ba213aee6cab2f4339b06a9
Jason Ho <eflyjason@gmail.com>
2016-03-16 13:34:02 +0800

    - change ViewController to PracticeFrontViewController

--- Run/PracticeFrontViewController.swift
+++ Run/PracticeFrontViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  PracticeFrontViewController.swift
 //  Run
 //
 //  Created by Jason Ho on 16/3/2016.
@@ -9,12 +9,12 @@
 import UIKit
 import CocoaLumberjack
 
-class ViewController: UIViewController {
+class PracticeFrontViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        DDLogVerbose("View Controller 之 super.viewDidLoad() 已加載")
+        DDLogInfo("Practice Front View Controller 之 super.viewDidLoad() 已加載")
     }
 
     override func didReceiveMemoryWarning() {
