5fa6adab511df4a7aecb4744a90c758808ad30f6
Jason Ho <eflyjason@gmail.com>
2016-03-18 14:55:15 +0800

    - add pod `CRToast`

--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment this line to define a global platform for your project
-platform :ios, '8.0'
+platform :ios, '9.0'
 # Uncomment this line if you're using Swift
 use_frameworks!
 
@@ -7,6 +7,7 @@ target 'Run' do
     pod 'AsyncSwift'
     pod 'Alamofire', '~> 3.0'
     pod 'CocoaLumberjack/Swift'
+    pod 'CRToast', '~> 0.0.7'
     pod 'MBProgressHUD', '~> 0.9.2'
     pod 'SwiftyJSON', :git => 'https://github.com/SwiftyJSON/SwiftyJSON.git'
     pod 'KLCPopup', :head
--- Pods/Target Support Files/Pods-Run/Pods-Run-frameworks.sh
+++ Pods/Target Support Files/Pods-Run/Pods-Run-frameworks.sh
@@ -86,6 +86,7 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-Run/Alamofire.framework"
   install_framework "Pods-Run/Async.framework"
+  install_framework "Pods-Run/CRToast.framework"
   install_framework "Pods-Run/CocoaLumberjack.framework"
   install_framework "Pods-Run/KLCPopup.framework"
   install_framework "Pods-Run/MBProgressHUD.framework"
@@ -94,6 +95,7 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-Run/Alamofire.framework"
   install_framework "Pods-Run/Async.framework"
+  install_framework "Pods-Run/CRToast.framework"
   install_framework "Pods-Run/CocoaLumberjack.framework"
   install_framework "Pods-Run/KLCPopup.framework"
   install_framework "Pods-Run/MBProgressHUD.framework"
