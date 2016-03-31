95321874e75b277f58be6315f99311a0e018e131
Jason Ho <eflyjason@gmail.com>
2016-03-31 14:19:11 +0800

    - add PracticeRecordViewController

---
+++ Run/PracticeRecordViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PracticeRecordViewController.swift
+//  Run
+//
+//  Created by Jason Ho on 31/3/2016.
+//  Copyright © 2016 Arefly. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import Async
+import Alamofire
+import CocoaLumberjack
+
+class PracticeRecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        DDLogInfo("Practice Record View Controller 之 super.viewDidLoad() 已加載")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
